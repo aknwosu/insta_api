100521db318803c8c11013d7ff513270af4e0921
Rebecca Nwosu <akunnarebecca@gmail.com>
2019-12-30 23:10:24 +0100

    dep, initialize mongo-driver

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hey from insta api")
+	// mongo
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	collection := client.Database("testing").Collection("numbers")
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	res, _ := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
+	id := res.InsertedID
+	fmt.Fprint(w, id)
 
 }
 
 func main() {
+
 	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
 }
